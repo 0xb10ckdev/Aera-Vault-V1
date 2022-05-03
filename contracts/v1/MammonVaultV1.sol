1a3874a32df802c54cb54314436ef9c7f5210cb9
Satoshi Naoki <astermiracle1009@gmail.com>
2022-05-04 03:48:44 +1000

    fix slither issues

--- contracts/v1/MammonVaultV1.sol
+++ contracts/v1/MammonVaultV1.sol
@@ -298,6 +298,9 @@ contract MammonVaultV1 is IMammonVaultV1, Ownable, ReentrancyGuard {
                 MAX_NOTICE_PERIOD
             );
         }
+        if (manager_ == address(0)) {
+            revert Mammon__ManagerIsZeroAddress();
+        }
 
         address[] memory managers = new address[](tokens.length);
         for (uint256 i = 0; i < tokens.length; i++) {
