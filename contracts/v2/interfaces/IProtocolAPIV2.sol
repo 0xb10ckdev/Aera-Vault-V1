a02e6b857a6ace1fea5dbba12e57373ba123ef3a
Satoshi Naoki <astermiracle1009@gmail.com>
2022-07-27 19:02:39 +1000

    add price determination

--- contracts/v2/interfaces/IProtocolAPIV2.sol
+++ contracts/v2/interfaces/IProtocolAPIV2.sol
@@ -5,4 +5,7 @@ pragma solidity 0.8.11;
 interface IProtocolAPIV2 {
     /// @notice Enable swap with oracle prices.
     function enableTradingWithOraclePrice() external;
+
+    /// @notice Enable or disable using oracle prices.
+    function setOraclesEnabled(bool enabled) external;
 }
