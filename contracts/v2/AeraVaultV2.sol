3662485bdb4d2746d8df244d3857adb2bf2b3c45
astermiracle <astermiracle1009@gmail.com>
2022-08-09 02:12:37 +1000

    run prettier

--- contracts/v2/AeraVaultV2.sol
+++ contracts/v2/AeraVaultV2.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
-
 import "../v1/dependencies/openzeppelin/SafeERC20.sol";
 import "../v1/dependencies/openzeppelin/IERC20.sol";
 import "../v1/dependencies/openzeppelin/Ownable.sol";
