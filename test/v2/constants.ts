76e2d7345d2f6590804a676ebfa5f7d34e351a1f
Satoshi Naoki <astermiracle1009@gmail.com>
2022-08-29 20:03:24 +1000

    update test with updated finalization process

--- test/v2/constants.ts
+++ test/v2/constants.ts
@@ -3,6 +3,7 @@ import { toWei } from "../v1/constants";
 export * from "../v1/constants";
 
 export const PRICE_DEVIATION = toWei(0.0001); // Deviation percentage of prices.
+export const MIN_FEE_DURATION = 2 * 30 * 24 * 60 * 60; // Minimum period to charge guarantee management fee.
 export const MIN_RELIABLE_VAULT_VALUE = toWei(1); // Minimum reliable vault TVL in base token
 export const MIN_SIGNIFICANT_DEPOSIT_VALUE = toWei(20); // Minimum significant deposit value. It will be measured in base token terms.
 export const MAX_ORACLE_SPOT_DIVERGENCE = toWei(1.1); // Maximum oracle spot price divergence.
