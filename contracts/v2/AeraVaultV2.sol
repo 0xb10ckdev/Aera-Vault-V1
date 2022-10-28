fa5f4123ced9fee3c7bcd0d560b9348af0a26416
Satoshi Naoki <astermiracle1009@gmail.com>
2022-10-28 12:00:02 +1000

    order imports alphabetically in AeraVaultV2

--- contracts/v2/AeraVaultV2.sol
+++ contracts/v2/AeraVaultV2.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
-import "../v1/dependencies/openzeppelin/SafeERC20.sol";
+import "../v1/dependencies/openzeppelin/ERC165Checker.sol";
 import "../v1/dependencies/openzeppelin/IERC20.sol";
+import "../v1/dependencies/openzeppelin/Math.sol";
 import "../v1/dependencies/openzeppelin/Ownable.sol";
 import "../v1/dependencies/openzeppelin/ReentrancyGuard.sol";
-import "../v1/dependencies/openzeppelin/Math.sol";
-import "../v1/dependencies/openzeppelin/ERC165Checker.sol";
-import "../v2/dependencies/openzeppelin/Multicall.sol";
-import "../v1/interfaces/IBManagedPoolFactory.sol";
+import "../v1/dependencies/openzeppelin/SafeERC20.sol";
+import "../v1/interfaces/IBManagedPool.sol";
 import "../v1/interfaces/IBManagedPoolController.sol";
+import "../v1/interfaces/IBManagedPoolFactory.sol";
 import "../v1/interfaces/IBMerkleOrchard.sol";
 import "../v1/interfaces/IBVault.sol";
-import "../v1/interfaces/IBManagedPool.sol";
 import "../v1/interfaces/IWithdrawalValidator.sol";
+import "../v2/dependencies/openzeppelin/Multicall.sol";
 import "./dependencies/chainlink/interfaces/AggregatorV2V3Interface.sol";
 import "./interfaces/IAeraVaultV2.sol";
 import "./OracleStorage.sol";
