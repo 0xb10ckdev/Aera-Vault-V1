e4f21d141e76a6ad5b47569ae0db7f9f1a2d0a8f
astermiracle <astermiracle1009@gmail.com>
2022-11-10 20:21:27 +1000

    update ERC4626Mock contract with custom maxWithdraw

--- contracts/v2/mocks/ERC4626Mock.sol
+++ contracts/v2/mocks/ERC4626Mock.sol
@@ -10,10 +10,26 @@ import "../dependencies/openzeppelin/ERC4626.sol";
  *      THIS CONTRACT IS FOR TESTING PURPOSES ONLY. DO NOT USE IN PRODUCTION.
  */
 contract ERC4626Mock is ERC4626 {
+    bool private useMaxWithdrawalAmount;
+    uint256 private maxWithdrawalAmount;
+ 
     // solhint-disable no-empty-blocks
     constructor(
         IERC20 asset_,
         string memory name_,
         string memory symbol_
     ) ERC4626(asset_) ERC20(name_, symbol_) {}
+
+    function maxWithdraw(address owner) public view virtual override returns (uint256) {
+        if (useMaxWithdrawalAmount) {
+            return maxWithdrawalAmount;
+        }
+
+        return _convertToAssets(balanceOf(owner), Math.Rounding.Down);
+    }
+
+    function setMaxWithdrawalAmount(uint256 amount, bool use) external {
+        maxWithdrawalAmount = amount;
+        useMaxWithdrawalAmount = use;
+    }
 }
