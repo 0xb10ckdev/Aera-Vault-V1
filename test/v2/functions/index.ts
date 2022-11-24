b60f991f28385a41ab9e05bf677a4e68f2b4f9ee
Satoshi Naoki <astermiracle1009@gmail.com>
2022-11-24 18:39:42 +1000

    refactor unit and integration tests with testAeraVaultV2

--- test/v2/functions/index.ts
+++ test/v2/functions/index.ts
@@ -1,23 +1,153 @@
-export { testDeployment } from "../functions/deployment";
-export { testFunctionCallsWhenNotInitialized } from "../functions/callFunctionsWhenNotInitialized";
-export { testFunctionCallsWhenFinalized } from "../functions/callFunctionsWhenFinalized";
-export { testInitialDeposit } from "../functions/initialDeposit";
-export { testDeposit } from "../functions/deposit";
-export { testDepositRiskingArbitrage } from "../functions/depositRiskingArbitrage";
-export { testWithdraw } from "../functions/withdraw";
-export { testDepositAndWithdraw } from "../functions/depositAndWithdraw";
-export { testUpdateWeightsGradually } from "../functions/updateWeightsGradually";
-export { testCancelWeightUpdates } from "./cancelWeightUpdates";
-export { testFinalize } from "../functions/finalize";
-export { testMulticall } from "../functions/multicall";
-export { testGetSpotPrices } from "../functions/getSpotPrices";
-export { testSweep } from "../functions/sweep";
-export { testClaimManagerFees } from "../functions/claimManagerFees";
-export { testSetManager } from "../functions/setManager";
-export { testSetOraclesEnabled } from "../functions/setOraclesEnabled";
-export { testEnableTradingRiskingArbitrage } from "../functions/enableTradingRiskingArbitrage";
-export { testEnableTradingWithWeights } from "../functions/enableTradingWithWeights";
-export { testEnableTradingWithOraclePrice } from "../functions/enableTradingWithOraclePrice";
-export { testDisableTrading } from "../functions/disableTrading";
-export { testSetSwapFee } from "../functions/setSwapFee";
-export { testOwnership } from "../functions/ownership";
+import { testFunctionCallsWhenNotInitialized } from "../functions/callFunctionsWhenNotInitialized";
+import { testFunctionCallsWhenFinalized } from "../functions/callFunctionsWhenFinalized";
+import { testInitialDeposit } from "../functions/initialDeposit";
+import { testDeposit } from "../functions/deposit";
+import { testDepositRiskingArbitrage } from "../functions/depositRiskingArbitrage";
+import { testWithdraw } from "../functions/withdraw";
+import { testDepositAndWithdraw } from "../functions/depositAndWithdraw";
+import { testUpdateWeightsGradually } from "../functions/updateWeightsGradually";
+import { testCancelWeightUpdates } from "./cancelWeightUpdates";
+import { testFinalize } from "../functions/finalize";
+import { testMulticall } from "../functions/multicall";
+import { testGetSpotPrices } from "../functions/getSpotPrices";
+import { testSweep } from "../functions/sweep";
+import { testClaimManagerFees } from "../functions/claimManagerFees";
+import { testSetManager } from "../functions/setManager";
+import { testSetOraclesEnabled } from "../functions/setOraclesEnabled";
+import { testEnableTradingRiskingArbitrage } from "../functions/enableTradingRiskingArbitrage";
+import { testEnableTradingWithWeights } from "../functions/enableTradingWithWeights";
+import { testEnableTradingWithOraclePrice } from "../functions/enableTradingWithOraclePrice";
+import { testDisableTrading } from "../functions/disableTrading";
+import { testSetSwapFee } from "../functions/setSwapFee";
+import { testOwnership } from "../functions/ownership";
+import {
+  normalizeWeights,
+  tokenValueArray,
+  tokenWithValues,
+  toUnit,
+  toWei,
+  valueArray,
+} from "../utils";
+import { ONE } from "../constants";
+
+export function testAeraVaultV2(): void {
+  describe("Aera Vault V2 Mainnet Functionality", function () {
+    describe("when Vault not initialized", function () {
+      describe("should be reverted to call functions", async function () {
+        testFunctionCallsWhenNotInitialized();
+      });
+
+      describe("initialize Vault", function () {
+        testInitialDeposit();
+      });
+    });
+
+    describe("when Vault is initialized", function () {
+      beforeEach(async function () {
+        for (let i = 0; i < this.tokens.length; i++) {
+          await this.tokens[i].approve(this.vault.address, toWei(100));
+        }
+
+        for (let i = 1; i < this.poolTokens.length; i++) {
+          await this.oracles[i].setLatestAnswer(toUnit(1, 8));
+        }
+
+        await this.vault.initialDeposit(
+          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
+          tokenWithValues(
+            this.tokenAddresses,
+            normalizeWeights(valueArray(ONE, this.tokens.length)),
+          ),
+        );
+      });
+
+      describe("when depositing to Vault", function () {
+        describe("with deposit function", async function () {
+          testDeposit();
+        });
+
+        describe("with depositRiskingArbitrage function", async function () {
+          testDepositRiskingArbitrage();
+        });
+      });
+
+      describe("when withdrawing from Vault", function () {
+        testWithdraw();
+      });
+
+      describe("when depositing and withdrawing", function () {
+        testDepositAndWithdraw();
+      });
+
+      describe("when call updateWeightsGradually()", function () {
+        testUpdateWeightsGradually();
+      });
+
+      describe("when call cancelWeightUpdates()", function () {
+        testCancelWeightUpdates();
+      });
+
+      describe("when finalize", function () {
+        describe("should be reverted to call functions when finalized", async () => {
+          testFunctionCallsWhenFinalized();
+        });
+
+        describe("initialize Vault", function () {
+          testFinalize();
+        });
+      });
+
+      describe("when enable/disable trading", function () {
+        describe("with enableTradingRiskingArbitrage function", function () {
+          testEnableTradingRiskingArbitrage();
+        });
+
+        describe("with enableTradingWithWeights function", function () {
+          testEnableTradingWithWeights();
+        });
+
+        describe("with enableTradingWithOraclePrice function", function () {
+          testEnableTradingWithOraclePrice();
+        });
+      });
+    });
+
+    describe("Multicall", function () {
+      testMulticall();
+    });
+
+    describe("Get Spot Prices", function () {
+      testGetSpotPrices();
+    });
+
+    describe("Sweep", function () {
+      testSweep();
+    });
+
+    describe("Claim Manager Fees", function () {
+      testClaimManagerFees();
+    });
+
+    describe("Update Elements", function () {
+      describe("Update Manager", function () {
+        testSetManager();
+      });
+
+      describe("Enable/Disable Oracle", function () {
+        testSetOraclesEnabled();
+      });
+
+      describe("Disable Trading", function () {
+        testDisableTrading();
+      });
+
+      describe("Set Swap Fee", function () {
+        testSetSwapFee();
+      });
+
+      describe("Ownership", function () {
+        testOwnership();
+      });
+    });
+  });
+}
