80210c88ecbe435f20a690655cc772d170f30812
Satoshi Naoki <astermiracle1009@gmail.com>
2022-11-24 12:24:57 +1000

    fix conflicts

--- test/v2/functions/initialDeposit.ts
+++ test/v2/functions/initialDeposit.ts
@@ -1,6 +1,5 @@
 import { expect } from "chai";
 import { BigNumber } from "ethers";
-import { ethers } from "hardhat";
 import { BALANCER_ERRORS, DEVIATION, ONE } from "../constants";
 import {
   normalizeWeights,
@@ -16,90 +15,6 @@ export function testInitialDeposit(): void {
     for (let i = 0; i < this.tokens.length; i++) {
       await this.tokens[i].approve(this.vault.address, toWei(2));
     }
-  });
-
-  describe("should be reverted to call functions", async function () {
-    it("when call deposit", async function () {
-      await expect(
-        this.vault.deposit(
-          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
-        ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call depositIfBalanceUnchanged", async function () {
-      await expect(
-        this.vault.depositIfBalanceUnchanged(
-          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
-        ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call depositRiskingArbitrage", async function () {
-      await expect(
-        this.vault.depositRiskingArbitrage(
-          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
-        ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call depositRiskingArbitrageIfBalanceUnchanged", async function () {
-      await expect(
-        this.vault.depositRiskingArbitrageIfBalanceUnchanged(
-          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
-        ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call withdraw", async function () {
-      await expect(
-        this.vault.withdraw(
-          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
-        ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call withdrawIfBalanceUnchanged", async function () {
-      await expect(
-        this.vault.withdrawIfBalanceUnchanged(
-          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
-        ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call updateWeightsGradually", async function () {
-      const blocknumber = await ethers.provider.getBlockNumber();
-      await expect(
-        this.vault
-          .connect(this.manager)
-          .updateWeightsGradually(
-            tokenWithValues(
-              this.tokenAddresses,
-              normalizeWeights(valueArray(ONE, this.tokens.length)),
-            ),
-            blocknumber + 1,
-            blocknumber + 1000,
-          ),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call cancelWeightUpdates", async function () {
-      await expect(
-        this.vault.connect(this.manager).cancelWeightUpdates(),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call claimManagerFees", async function () {
-      await expect(
-        this.vault.connect(this.manager).claimManagerFees(),
-      ).to.be.revertedWith("Aera__VaultNotInitialized");
-    });
-
-    it("when call finalize", async function () {
-      await expect(this.vault.finalize()).to.be.revertedWith(
-        "Aera__VaultNotInitialized",
-      );
-    });
   });
 
   describe("should be reverted to initialize the vault", async function () {
@@ -189,6 +104,26 @@ export function testInitialDeposit(): void {
           ),
         ),
       ).to.be.revertedWith(BALANCER_ERRORS.ZERO_INVARIANT);
+    });
+
+    it("when vault is already initialized ", async function () {
+      await this.vault.initialDeposit(
+        tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
+        tokenWithValues(
+          this.tokenAddresses,
+          normalizeWeights(valueArray(ONE, this.tokens.length)),
+        ),
+      );
+
+      await expect(
+        this.vault.initialDeposit(
+          tokenValueArray(this.tokenAddresses, ONE, this.tokens.length),
+          tokenWithValues(
+            this.tokenAddresses,
+            normalizeWeights(valueArray(ONE, this.tokens.length)),
+          ),
+        ),
+      ).to.be.revertedWith("Aera__VaultIsAlreadyInitialized");
     });
   });
 
