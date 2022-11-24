c8dd30b82f7d2fd15146666df827ce211a601b36
astermiracle <astermiracle1009@gmail.com>
2022-11-24 19:43:33 +1000

    update tests with setup vault

--- test/v2/fixtures.ts
+++ test/v2/fixtures.ts
@@ -1,12 +1,304 @@
-import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
+import hre, { ethers } from "hardhat";
+import { writeFile, rm } from "fs/promises";
 import {
+  AeraVaultV2Mock,
+  AeraVaultV2Mock__factory,
+  BalancerVaultMock__factory,
+  BaseManagedPoolFactory__factory,
   ERC4626Mock,
   ERC4626Mock__factory,
+  IERC20,
+  ManagedPoolFactory,
+  ManagedPoolFactory__factory,
   OracleMock,
   OracleMock__factory,
+  WithdrawalValidatorMock,
+  WithdrawalValidatorMock__factory,
 } from "../../typechain";
+import {
+  MAX_MANAGEMENT_FEE,
+  MAX_ORACLE_DELAY,
+  MAX_ORACLE_SPOT_DIVERGENCE,
+  MIN_FEE_DURATION,
+  MIN_RELIABLE_VAULT_VALUE,
+  MIN_SIGNIFICANT_DEPOSIT_VALUE,
+  MIN_SWAP_FEE,
+  ONE,
+  ZERO_ADDRESS,
+} from "./constants";
+import { toWei, valueArray } from "./utils";
+import { getConfig } from "../../scripts/config";
+import { setupTokens } from "../v1/fixtures";
 
 export * from "../v1/fixtures";
+
+export type DeployedData = {
+  admin: SignerWithAddress;
+  manager: SignerWithAddress;
+  user: SignerWithAddress;
+  tokens: IERC20[];
+  tokenAddresses: string[];
+  poolTokens: IERC20[];
+  yieldTokens: ERC4626Mock[];
+  sortedTokens: string[];
+  unsortedTokens: string[];
+  underlyingIndexes: number[];
+  oracles: OracleMock[];
+  oracleAddresses: string[];
+  validator: WithdrawalValidatorMock;
+  factory: ManagedPoolFactory;
+  vault: AeraVaultV2Mock;
+};
+
+export const setupAssetContracts = async (): Promise<{
+  admin: SignerWithAddress;
+  manager: SignerWithAddress;
+  user: SignerWithAddress;
+  tokens: IERC20[];
+  poolTokens: IERC20[];
+  yieldTokens: ERC4626Mock[];
+  sortedTokens: string[];
+  unsortedTokens: string[];
+  tokenAddresses: string[];
+  underlyingIndexes: number[];
+  oracles: OracleMock[];
+  oracleAddresses: string[];
+  validator: WithdrawalValidatorMock;
+}> => {
+  const { admin, manager, user } = await ethers.getNamedSigners();
+  const {
+    tokens: poolTokens,
+    sortedTokens,
+    unsortedTokens: unsortedPoolTokens,
+  } = await setupTokens();
+  const yieldTokens = await setupYieldBearingAssets(sortedTokens.slice(0, 2));
+  const underlyingIndexes = [0, 1];
+  const oracles = await setupOracles();
+
+  const tokens = [...poolTokens, ...yieldTokens];
+  const tokenAddresses = tokens.map(token => token.address);
+  const unsortedTokens = [
+    ...unsortedPoolTokens,
+    ...yieldTokens.map(token => token.address),
+  ];
+  const oracleAddresses = oracles.map((oracle: OracleMock) => oracle.address);
+  oracleAddresses[0] = ZERO_ADDRESS;
+
+  await Promise.all(
+    yieldTokens.map((token, index) =>
+      poolTokens[index].approve(token.address, toWei("100000")),
+    ),
+  );
+  await Promise.all(
+    yieldTokens.map(token => token.deposit(toWei("100000"), admin.address)),
+  );
+
+  const validatorMock =
+    await ethers.getContractFactory<WithdrawalValidatorMock__factory>(
+      "WithdrawalValidatorMock",
+    );
+
+  const validator = await validatorMock.connect(admin).deploy(tokens.length);
+
+  return {
+    admin,
+    manager,
+    user,
+    tokens,
+    poolTokens,
+    yieldTokens,
+    sortedTokens,
+    unsortedTokens,
+    tokenAddresses,
+    underlyingIndexes,
+    oracles,
+    oracleAddresses,
+    validator,
+  };
+};
+
+export const setupVaultWithBalancerVaultMock =
+  async (): Promise<DeployedData> => {
+    const {
+      admin,
+      manager,
+      user,
+      tokens,
+      poolTokens,
+      yieldTokens,
+      sortedTokens,
+      unsortedTokens,
+      tokenAddresses,
+      underlyingIndexes,
+      oracles,
+      oracleAddresses,
+      validator,
+    } = await setupAssetContracts();
+
+    const baseManagedPoolFactoryContract =
+      await ethers.getContractFactory<BaseManagedPoolFactory__factory>(
+        "BaseManagedPoolFactory",
+      );
+    const managedPoolFactoryContract =
+      await ethers.getContractFactory<ManagedPoolFactory__factory>(
+        "ManagedPoolFactory",
+      );
+
+    const bVaultContract =
+      await ethers.getContractFactory<BalancerVaultMock__factory>(
+        "BalancerVaultMock",
+      );
+    const bVault = await bVaultContract.connect(admin).deploy(ZERO_ADDRESS);
+    const baseManagedPoolFactory = await baseManagedPoolFactoryContract
+      .connect(admin)
+      .deploy(bVault.address);
+    const factory = await managedPoolFactoryContract
+      .connect(admin)
+      .deploy(baseManagedPoolFactory.address);
+
+    const validWeights = valueArray(
+      ONE.div(poolTokens.length),
+      poolTokens.length,
+    );
+
+    const vaultFactory =
+      await ethers.getContractFactory<AeraVaultV2Mock__factory>(
+        "AeraVaultV2Mock",
+      );
+    const vault = await vaultFactory.connect(admin).deploy({
+      factory: factory.address,
+      name: "Test",
+      symbol: "TEST",
+      poolTokens: sortedTokens,
+      weights: validWeights,
+      oracles: oracleAddresses,
+      yieldTokens: yieldTokens.map((token, index) => ({
+        token: token.address,
+        underlyingIndex: index,
+      })),
+      numeraireAssetIndex: 0,
+      swapFeePercentage: MIN_SWAP_FEE,
+      manager: manager.address,
+      validator: validator.address,
+      minReliableVaultValue: MIN_RELIABLE_VAULT_VALUE,
+      minSignificantDepositValue: MIN_SIGNIFICANT_DEPOSIT_VALUE,
+      maxOracleSpotDivergence: MAX_ORACLE_SPOT_DIVERGENCE,
+      maxOracleDelay: MAX_ORACLE_DELAY,
+      minFeeDuration: MIN_FEE_DURATION,
+      managementFee: MAX_MANAGEMENT_FEE,
+      merkleOrchard: ZERO_ADDRESS,
+      description: "Test vault description",
+    });
+
+    return {
+      admin,
+      manager,
+      user,
+      vault,
+      validator,
+      factory,
+      poolTokens,
+      tokens,
+      tokenAddresses,
+      yieldTokens,
+      underlyingIndexes,
+      sortedTokens,
+      oracles,
+      oracleAddresses,
+      unsortedTokens,
+    };
+  };
+
+export const setupVaultWithBalancerVault = async (): Promise<DeployedData> => {
+  const config = getConfig(hre.network.config.chainId || 1);
+  const {
+    admin,
+    manager,
+    user,
+    tokens,
+    poolTokens,
+    yieldTokens,
+    sortedTokens,
+    unsortedTokens,
+    tokenAddresses,
+    underlyingIndexes,
+    oracles,
+    oracleAddresses,
+    validator,
+  } = await setupAssetContracts();
+
+  const baseManagedPoolFactoryContract =
+    await ethers.getContractFactory<BaseManagedPoolFactory__factory>(
+      "BaseManagedPoolFactory",
+    );
+  const managedPoolFactoryContract =
+    await ethers.getContractFactory<ManagedPoolFactory__factory>(
+      "ManagedPoolFactory",
+    );
+
+  const baseManagedPoolFactory = await baseManagedPoolFactoryContract
+    .connect(admin)
+    .deploy(config.bVault);
+  const factory = await managedPoolFactoryContract
+    .connect(admin)
+    .deploy(baseManagedPoolFactory.address);
+
+  const validWeights = valueArray(
+    ONE.div(poolTokens.length),
+    poolTokens.length,
+  );
+
+  await writeFile(
+    ".testConfig.json",
+    JSON.stringify({
+      factory: factory.address,
+      name: "Test",
+      symbol: "TEST",
+      poolTokens: sortedTokens,
+      weights: validWeights,
+      oracles: oracleAddresses,
+      yieldTokens: yieldTokens.map(token => token.address),
+      numeraireAssetIndex: 0,
+      swapFeePercentage: MIN_SWAP_FEE,
+      manager: manager.address,
+      validator: validator.address,
+      minReliableVaultValue: MIN_RELIABLE_VAULT_VALUE,
+      minSignificantDepositValue: MIN_SIGNIFICANT_DEPOSIT_VALUE,
+      maxOracleSpotDivergence: MAX_ORACLE_SPOT_DIVERGENCE,
+      maxOracleDelay: MAX_ORACLE_DELAY,
+      minFeeDuration: MIN_FEE_DURATION,
+      managementFee: MAX_MANAGEMENT_FEE,
+      description: "Test vault description",
+    }),
+  );
+
+  const vault = await hre.run("deploy:vaultV2", {
+    configPath: ".testConfig.json",
+    silent: true,
+    test: true,
+  });
+
+  await rm(".testConfig.json");
+
+  return {
+    admin,
+    manager,
+    user,
+    vault,
+    validator,
+    factory,
+    poolTokens,
+    tokens,
+    tokenAddresses,
+    yieldTokens,
+    underlyingIndexes,
+    sortedTokens,
+    oracles,
+    oracleAddresses,
+    unsortedTokens,
+  };
+};
 
 export const setupOracles = async (
   length: number = 4,
--- test/v2/unit/AeraVaultV2.ts
+++ test/v2/unit/AeraVaultV2.ts
@@ -1,202 +1,30 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
 import { ethers } from "hardhat";
-import {
-  BalancerVaultMock__factory,
-  IERC20,
-  ERC4626Mock,
-  BaseManagedPoolFactory__factory,
-  ManagedPoolFactory,
-  ManagedPoolFactory__factory,
-  AeraVaultV2Mock,
-  AeraVaultV2Mock__factory,
-  WithdrawalValidatorMock,
-  WithdrawalValidatorMock__factory,
-  OracleMock,
-} from "../../../typechain";
-import {
-  MAX_MANAGEMENT_FEE,
-  MIN_SWAP_FEE,
-  ONE,
-  ZERO_ADDRESS,
-  MIN_FEE_DURATION,
-  MIN_RELIABLE_VAULT_VALUE,
-  MIN_SIGNIFICANT_DEPOSIT_VALUE,
-  MAX_ORACLE_SPOT_DIVERGENCE,
-  MAX_ORACLE_DELAY,
-} from "../constants";
-import {
-  setupTokens,
-  setupOracles,
-  setupYieldBearingAssets,
-} from "../fixtures";
-import { toWei, valueArray } from "../utils";
+import { setupVaultWithBalancerVaultMock } from "../fixtures";
 import { testAeraVaultV2 } from "../functions";
 
-describe("Aera Vault V2 Mainnet Functionality", function () {
-  let admin: SignerWithAddress;
-  let manager: SignerWithAddress;
-  let user: SignerWithAddress;
-  let vault: AeraVaultV2Mock;
-  let validator: WithdrawalValidatorMock;
-  let factory: ManagedPoolFactory;
-  let poolTokens: IERC20[];
-  let tokens: IERC20[];
-  let tokenAddresses: string[];
-  let yieldTokens: ERC4626Mock[];
-  let underlyingIndexes: number[];
-  let sortedTokens: string[];
-  let unsortedTokens: string[];
-  let oracles: OracleMock[];
-  let oracleAddresses: string[];
+describe("Unit Test", function () {
   let snapshot: unknown;
-
-  const getUserBalances = async (address: string) => {
-    const balances = await Promise.all(
-      tokens.map(token => token.balanceOf(address)),
-    );
-    return balances;
-  };
-
-  const getManagersFeeTotal = async function () {
-    const managersFeeTotal = await Promise.all(
-      Array.from(Array(tokens.length).keys()).map(index =>
-        vault.managersFeeTotal(index),
-      ),
-    );
-    return managersFeeTotal;
-  };
-
-  const getState = async (
-    managerAddress: string | null = null,
-    adminAddress: string | null = null,
-  ) => {
-    const [holdings, adminBalances, managerBalances] = await Promise.all([
-      vault.getHoldings(),
-      getUserBalances(adminAddress || admin.address),
-      getUserBalances(managerAddress || manager.address),
-    ]);
-
-    return {
-      holdings,
-      adminBalances,
-      managerBalances,
-    };
-  };
 
   beforeEach(async function () {
     snapshot = await ethers.provider.send("evm_snapshot", []);
 
-    ({ admin, manager, user } = await ethers.getNamedSigners());
-    ({
-      tokens: poolTokens,
-      sortedTokens,
-      unsortedTokens,
-    } = await setupTokens());
-    yieldTokens = await setupYieldBearingAssets(sortedTokens.slice(0, 2));
-    underlyingIndexes = [0, 1];
-    oracles = await setupOracles();
+    const deployedData = await setupVaultWithBalancerVaultMock();
 
-    tokens = [...poolTokens, ...yieldTokens];
-    tokenAddresses = tokens.map(token => token.address);
-    unsortedTokens = [
-      ...unsortedTokens,
-      ...yieldTokens.map(token => token.address),
-    ];
-    oracleAddresses = oracles.map((oracle: OracleMock) => oracle.address);
-    oracleAddresses[0] = ZERO_ADDRESS;
-
-    await Promise.all(
-      yieldTokens.map((token, index) =>
-        poolTokens[index].approve(token.address, toWei("100000")),
-      ),
-    );
-    await Promise.all(
-      yieldTokens.map(token => token.deposit(toWei("100000"), admin.address)),
-    );
-
-    const validatorMock =
-      await ethers.getContractFactory<WithdrawalValidatorMock__factory>(
-        "WithdrawalValidatorMock",
-      );
-
-    validator = await validatorMock.connect(admin).deploy(tokens.length);
-
-    const bVaultContract =
-      await ethers.getContractFactory<BalancerVaultMock__factory>(
-        "BalancerVaultMock",
-      );
-    const bVault = await bVaultContract.connect(admin).deploy(ZERO_ADDRESS);
-
-    const baseManagedPoolFactoryContract =
-      await ethers.getContractFactory<BaseManagedPoolFactory__factory>(
-        "BaseManagedPoolFactory",
-      );
-    const baseManagedPoolFactory = await baseManagedPoolFactoryContract
-      .connect(admin)
-      .deploy(bVault.address);
-
-    const managedPoolFactoryContract =
-      await ethers.getContractFactory<ManagedPoolFactory__factory>(
-        "ManagedPoolFactory",
-      );
-    factory = await managedPoolFactoryContract
-      .connect(admin)
-      .deploy(baseManagedPoolFactory.address);
-
-    const validWeights = valueArray(
-      ONE.div(poolTokens.length),
-      poolTokens.length,
-    );
-
-    const vaultFactory =
-      await ethers.getContractFactory<AeraVaultV2Mock__factory>(
-        "AeraVaultV2Mock",
-      );
-    vault = await vaultFactory.connect(admin).deploy({
-      factory: factory.address,
-      name: "Test",
-      symbol: "TEST",
-      poolTokens: sortedTokens,
-      weights: validWeights,
-      oracles: oracleAddresses,
-      yieldTokens: yieldTokens.map((token, index) => ({
-        token: token.address,
-        underlyingIndex: index,
-      })),
-      numeraireAssetIndex: 0,
-      swapFeePercentage: MIN_SWAP_FEE,
-      manager: manager.address,
-      validator: validator.address,
-      minReliableVaultValue: MIN_RELIABLE_VAULT_VALUE,
-      minSignificantDepositValue: MIN_SIGNIFICANT_DEPOSIT_VALUE,
-      maxOracleSpotDivergence: MAX_ORACLE_SPOT_DIVERGENCE,
-      maxOracleDelay: MAX_ORACLE_DELAY,
-      minFeeDuration: MIN_FEE_DURATION,
-      managementFee: MAX_MANAGEMENT_FEE,
-      merkleOrchard: ZERO_ADDRESS,
-      description: "Test vault description",
-    });
-
-    this.admin = admin;
-    this.manager = manager;
-    this.user = user;
-    this.vault = vault;
-    this.validator = validator;
-    this.factory = factory;
-    this.poolTokens = poolTokens;
-    this.tokens = tokens;
-    this.tokenAddresses = tokenAddresses;
-    this.yieldTokens = yieldTokens;
-    this.underlyingIndexes = underlyingIndexes;
-    this.sortedTokens = sortedTokens;
-    this.oracles = oracles;
-    this.oracleAddresses = oracleAddresses;
-    this.unsortedTokens = unsortedTokens;
-    this.snapshot = snapshot;
-
-    this.getUserBalances = getUserBalances;
-    this.getState = getState;
-    this.getManagersFeeTotal = getManagersFeeTotal;
+    this.admin = deployedData.admin;
+    this.manager = deployedData.manager;
+    this.user = deployedData.user;
+    this.vault = deployedData.vault;
+    this.validator = deployedData.validator;
+    this.factory = deployedData.factory;
+    this.poolTokens = deployedData.poolTokens;
+    this.tokens = deployedData.tokens;
+    this.tokenAddresses = deployedData.tokenAddresses;
+    this.yieldTokens = deployedData.yieldTokens;
+    this.underlyingIndexes = deployedData.underlyingIndexes;
+    this.sortedTokens = deployedData.sortedTokens;
+    this.oracles = deployedData.oracles;
+    this.oracleAddresses = deployedData.oracleAddresses;
+    this.unsortedTokens = deployedData.unsortedTokens;
   });
 
   afterEach(async function () {
