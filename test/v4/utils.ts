a7ec375193aa289725348429233f2ae5700c66e7
Satoshi Naoki <astermiracle1009@gmail.com>
2022-12-02 00:27:37 +1000

    move test for ManagerWhitelist from v1 to v4

---
+++ test/v4/utils.ts
@@ -0,0 +1,20 @@
+import { Signer } from "ethers";
+import { ethers } from "hardhat";
+import {
+  ManagerWhitelist,
+  ManagerWhitelist__factory,
+} from "../../typechain";
+
+export * from "../v1/utils";
+
+export const deployManagerWhitelist = async (
+  signer: Signer,
+  managers: string[],
+): Promise<ManagerWhitelist> => {
+  const managerWhitelist =
+    await ethers.getContractFactory<ManagerWhitelist__factory>(
+      "ManagerWhitelist",
+    );
+
+  return await managerWhitelist.connect(signer).deploy(managers);
+};
