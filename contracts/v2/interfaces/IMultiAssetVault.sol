259d5e04d97a862f312d3dc777406459ec13ea16
Satoshi Naoki <astermiracle1009@gmail.com>
2022-12-20 02:49:11 +1000

    remove returned variable name from IMultiAssetVault to fix shadowing issue

--- contracts/v2/interfaces/IMultiAssetVault.sol
+++ contracts/v2/interfaces/IMultiAssetVault.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.11;
 /// @title Multi-asset vault interface.
 interface IMultiAssetVault {
     /// @notice Balance of token with given index.
-    /// @return holding Current token balance in Balancer Pool and Aera Vault.
-    function holding(uint256 index) external view returns (uint256 holding);
+    /// @return Current token balance in Balancer Pool and Aera Vault.
+    function holding(uint256 index) external view returns (uint256);
 
     /// @notice Return balance of pool tokens and yield tokens.
-    /// @return holdings Current token balances.
-    function getHoldings() external view returns (uint256[] memory holdings);
+    /// @return Current token balances.
+    function getHoldings() external view returns (uint256[] memory);
 }
