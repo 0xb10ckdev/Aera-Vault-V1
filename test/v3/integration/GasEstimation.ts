493ae31f89fa77bc079c073ba5d19ab415cf07cb
Satoshi Naoki <astermiracle1009@gmail.com>
2023-01-20 00:50:36 +1000

    add detailed gas estimation for v3 approach

--- test/v3/integration/GasEstimation.ts
+++ test/v3/integration/GasEstimation.ts
@@ -1,4 +1,4 @@
-import { baseContext } from "../../shared/contexts";
+import { ethers } from "hardhat";
 import { setupVaultWithBalancerVault } from "../fixtures";
 import { ONE } from "../../v1/constants";
 import {
@@ -17,25 +17,31 @@ import {
   valueArray,
 } from "../utils";
 
-baseContext("Gas estimation for new rebalancing approach", function () {
+describe("Gas estimation for new rebalancing approach", function () {
   let erc20Tokens: ERC20Mock[];
   let tokens: IERC20[];
   let tokenAddresses: string[];
   let oracles: OracleMock[];
   let vault: AeraVaultV3Mock;
-  const gasEstimation: { [method: string]: number } = {};
+  let snapshot: unknown;
+  let gasEstimation: { [method: string]: any } = {};
 
-  before(async function () {
-    erc20Tokens = await this.loadFixture(setupTokens);
-    ({ tokens, tokenAddresses, oracles, vault } = await this.loadFixture(
-      setupVaultWithBalancerVault,
-    ));
+  beforeEach(async function () {
+    snapshot = await ethers.provider.send("evm_snapshot", []);
+
+    erc20Tokens = await setupTokens();
+    ({ tokens, tokenAddresses, oracles, vault } =
+      await setupVaultWithBalancerVault());
+
+    for (let i = 0; i < 20; i++) {
+      await erc20Tokens[i].transfer(vault.address, ONE);
+    }
 
     for (let i = 0; i < tokens.length; i++) {
       await tokens[i].approve(vault.address, toWei(100));
     }
 
-    for (let i = 1; i < this.numPoolTokens; i++) {
+    for (let i = 1; i < oracles.length; i++) {
       await oracles[i].setLatestAnswer(toUnit(1, 8));
     }
 
@@ -48,12 +54,20 @@ baseContext("Gas estimation for new rebalancing approach", function () {
     );
   });
 
-  after(function () {
-    console.log("Gas Estimation");
-    console.table(gasEstimation);
+  afterEach(async function () {
+    await ethers.provider.send("evm_revert", [snapshot]);
   });
 
   describe("Approve Tokens", function () {
+    before(function () {
+      gasEstimation = {};
+    });
+
+    after(function () {
+      console.log("Approve Tokens");
+      console.table(gasEstimation);
+    });
+
     it("should be possible to approve tokens", async function () {
       let estimation = 0;
 
@@ -67,30 +81,77 @@ baseContext("Gas estimation for new rebalancing approach", function () {
     });
   });
 
-  describe("Bind Tokens", function () {
-    it("should be possible to bind tokens", async function () {
-      for (let i = 0; i < 3; i++) {
-        await erc20Tokens[i].transfer(vault.address, ONE);
-      }
-
-      gasEstimation["Bind and deposit 3 tokens"] = (
-        await vault.estimateGas.depositAndBindTokens(
-          erc20Tokens.slice(0, 3).map(token => token.address),
-        )
-      ).toNumber();
+  describe("Bind Tokens", async function () {
+    before(function () {
+      gasEstimation = {};
     });
+
+    after(function () {
+      console.log("Bind Tokens");
+      console.table(gasEstimation);
+    });
+
+    for (let i = 0; i <= 4; i += 2) {
+      describe(`when pool has ${4 + i} tokens`, async function () {
+        beforeEach(async function () {
+          await vault.depositAndBindTokens(
+            erc20Tokens.slice(16, 16 + i).map(token => token.address),
+          );
+        });
+
+        for (let j = 3; j <= 15; j += 3) {
+          it(`should be possible to bind ${j} tokens`, async function () {
+            const estimation = (
+              await vault.estimateGas.depositAndBindTokens(
+                erc20Tokens.slice(0, j).map(token => token.address),
+              )
+            ).toNumber();
+            if (!gasEstimation[`Bind and deposit ${j} tokens`]) {
+              gasEstimation[`Bind and deposit ${j} tokens`] = {};
+            }
+            gasEstimation[`Bind and deposit ${j} tokens`][
+              `when pool has ${4 + i} tokens`
+            ] = estimation;
+          });
+        }
+      });
+    }
   });
 
   describe("Unbind Tokens", function () {
-    it("should be possible to unbind tokens", async function () {
-      await vault.depositAndBindTokens(
-        erc20Tokens.slice(0, 3).map(token => token.address),
-      );
-      gasEstimation["Withdraw and unbind 3 tokens"] = (
-        await vault.estimateGas.unbindAndWithdrawTokens(
-          tokens.slice(0, 3).map(token => token.address),
-        )
-      ).toNumber();
+    before(function () {
+      gasEstimation = {};
     });
+
+    after(function () {
+      console.log("Unbind Tokens");
+      console.table(gasEstimation);
+    });
+
+    for (let i = 3; i <= 15; i += 3) {
+      describe(`when pool has ${4 + i} tokens`, async function () {
+        beforeEach(async function () {
+          await vault.depositAndBindTokens(
+            erc20Tokens.slice(0, i).map(token => token.address),
+          );
+        });
+
+        for (let j = 3; j <= i; j += 3) {
+          it(`should be possible to unbind ${j} tokens`, async function () {
+            const estimation = (
+              await vault.estimateGas.unbindAndWithdrawTokens(
+                erc20Tokens.slice(0, j).map(token => token.address),
+              )
+            ).toNumber();
+            if (!gasEstimation[`Withdraw and unbind ${j} tokens`]) {
+              gasEstimation[`Withdraw and unbind ${j} tokens`] = {};
+            }
+            gasEstimation[`Withdraw and unbind ${j} tokens`][
+              `when pool has ${4 + i} tokens`
+            ] = estimation;
+          });
+        }
+      });
+    }
   });
 });
