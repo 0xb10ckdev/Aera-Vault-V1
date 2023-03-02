68af0b59489eaeb5c9b04febcfa3a0325d4fa664
Ben <ben.schreck@gauntlet.network>
2023-03-02 14:07:15 -0800

    tests passing, coverage still a problem

--- .solcover.js
+++ .solcover.js
@@ -9,12 +9,13 @@ if (!mnemonic) {
 module.exports = {
   istanbulReporter: ["html", "lcov"],
   onCompileComplete: async function (_config) {
-    await run("typechain");
+    // these typechain commands are not working for me
+    //await run("typechain");
   },
   onIstanbulComplete: async function (_config) {
     // We need to do this because solcover generates bespoke artifacts.
     shell.rm("-rf", "./artifacts");
-    shell.rm("-rf", "./typechain");
+    //shell.rm("-rf", "./typechain");
   },
   providerOptions: {
     mnemonic,
